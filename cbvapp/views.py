59d95c13bdc9c243becbbd72aaaed1e6c53a2827
pradeepmaurya <pm9231@gmail.com>
2022-03-24 10:45:32 +0530

    mixins used for GET and POST method

--- cbvapp/views.py
+++ cbvapp/views.py
@@ -5,8 +5,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import Http404
 from rest_framework  import status
+from rest_framework import mixins, generics
 
 
+class Books_list_View(mixins.ListModelMixin,mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Books.objects.all()
+    serializer_class = SerializerBooks
+    def get(self, request):
+        return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+    
+class Books_details_view(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    pass
+
+"""
 class Books_list_View(APIView):
     
     def get(self, request):
@@ -48,3 +62,5 @@ class Books_details_view(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_304_NOT_MODIFIED)
+
+"""
